67bf9941d4fd7fbfac17dde3d7790b7586e998a0
unknown <euphoriauniverse0716@gmail.com>
2026-01-13 17:35:27 +0900

    권채운 그라운드

--- src/pages/Ground.jsx
+++ src/pages/Ground.jsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import React from "react";
+import "./Ground.css";
 
 const Ground = () => {
   return (
-    <div>Ground</div>
-  )
-}
+    <section className="Ground">
+      <img
+        className="ground_bg"
+        src="/img/ground_main_img.jpg"
+        alt="그라운드 배경"
+      />
+      <div className="inner">
+        <div className="ground_content">
+          <h1 className="ground_title">KIA TIGERS</h1>
 
-export default Ground
+          {/* sns버튼 */}
+          <div className="ground_actions">
+            <a href="#">
+              <img src="" alt="" />
+            </a>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Ground;
