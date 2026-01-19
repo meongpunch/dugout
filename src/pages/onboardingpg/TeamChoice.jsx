98f0f4b9ea338c6de181e74c118f371ccd6c60d9
김의성 <v0514v@naver.com>
2026-01-19 14:50:44 +0900

    app.jsx추가, 온보딩탑바 추가, 온보딩 수정

--- src/pages/onboardingpg/TeamChoice.jsx
+++ src/pages/onboardingpg/TeamChoice.jsx
@@ -1,38 +1,20 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import './TeamChoice.css';
+import OnboardingTopBar from '../../components/OnboardingTopBar';
+
 
 const TEAMS = [
   { id: 'ssg', name: 'SSG 랜더스', logo: '/img/onboarding-logo-ssg.svg' },
-  {
-    id: 'kiwoom',
-    name: '키움 히어로즈',
-    logo: '/img/onboarding-logo-kiwoom.svg',
-  },
+  { id: 'kiwoom', name: '키움 히어로즈', logo: '/img/onboarding-logo-kiwoom.svg' },
   { id: 'lg', name: 'LG 트윈스', logo: '/img/onboarding-logo-lg.svg' },
   { id: 'kt', name: 'KT 위즈', logo: '/img/onboarding-logo-kt.svg' },
   { id: 'kia', name: 'KIA 타이거즈', logo: '/img/onboarding-logo-kia.svg' },
   { id: 'nc', name: 'NC 다이노스', logo: '/img/onboarding-logo-nc.svg' },
-  {
-    id: 'samsung',
-    name: '삼성 라이온즈',
-    logo: '/img/onboarding-logo-samsung.svg',
-  },
-  {
-    id: 'lotte',
-    name: '롯데 자이언츠',
-    logo: '/img/onboarding-logo-lotte.svg',
-  },
-  {
-    id: 'doosan',
-    name: '두산 베어스',
-    logo: '/img/onboarding-logo-doosan.svg',
-  },
-  {
-    id: 'hanwha',
-    name: '한화 이글스',
-    logo: '/img/onboarding-logo-hanwha.svg',
-  },
+  { id: 'samsung', name: '삼성 라이온즈', logo: '/img/onboarding-logo-samsung.svg' },
+  { id: 'lotte', name: '롯데 자이언츠', logo: '/img/onboarding-logo-lotte.svg' },
+  { id: 'doosan', name: '두산 베어스', logo: '/img/onboarding-logo-doosan.svg' },
+  { id: 'hanwha', name: '한화 이글스', logo: '/img/onboarding-logo-hanwha.svg' },
 ];
 
 export default function TeamChoice() {
@@ -56,30 +38,24 @@ export default function TeamChoice() {
   return (
     <div className="TeamChoice-container">
       <div className="inner">
-        {/* Header */}
-        <div className="TeamchoiceHeader">
-          <div className="innerHeader">
-            <button
-              className="back-btn"
-              type="button"
-              onClick={() => navigate(-1)}
-              aria-label="뒤로가기"
-            >
-              <img src="/img/onboarding-back-icon.svg" alt="" />
-            </button>
-          </div>
+        {/* ✅ Header: back은 login으로 */}
+        <OnboardingTopBar
+          iconType="back"
+          skipType="none"
+          onBack={() => navigate('/login')}
+        />
+
+        {/* ✅ Title은 픽스드(스크롤 밖) */}
+        <div className="contents">
+          <h2>
+            이제, 10번째 선수로 <br />
+            계약할 시간입니다
+          </h2>
+          <h3>당신의 심장이 뛰는 구단을 선택해 주세요.</h3>
         </div>
 
-        {/* Scroll Area */}
+        {/* ✅ 팀선택 카드만 스크롤 */}
         <div className="scrollArea">
-          <div className="contents">
-            <h2>
-              이제, 10번째 선수로 <br />
-              계약할 시간입니다
-            </h2>
-            <h3>당신의 심장이 뛰는 구단을 선택해 주세요.</h3>
-          </div>
-
           <div className="choice-box">
             {TEAMS.map((team) => {
               const isSelected = selectedTeam?.id === team.id;
@@ -93,11 +69,7 @@ export default function TeamChoice() {
                   aria-pressed={isSelected}
                 >
                   <div className="team-logo-wrap">
-                    <img
-                      className="team-logo"
-                      src={team.logo}
-                      alt={team.name}
-                    />
+                    <img className="team-logo" src={team.logo} alt={team.name} />
                   </div>
                   <p className="team-name">{team.name}</p>
                   {isSelected && <span className="selected-dot" />}
